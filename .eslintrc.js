1ec9e936ab5f34956bb6a796d968893ae13f5e8e
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2021-09-01 12:19:34 -0700

    Init

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+	extends: 'react-app',
+	plugins: ['simple-import-sort'],
+	rules: {
+		'simple-import-sort/imports': [
+			'error',
+			{
+				groups: [
+					['^\\u0000'],
+					['^react$', '^react', '^@?\\w'],
+					['^(.data-landing)(/.*)'],
+					['^(@root|types|data)(/.*)'],
+					['^(pages)(/.*)'],
+					['^(components|enums|images|loaders|utils|context)(/.*)'],
+					[('^\\.\\.(?!/?$)', '^\\.\\./?$')],
+					['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+				],
+			},
+		],
+		'sort-imports': 'off',
+		'import/order': 'off',
+	},
+}
